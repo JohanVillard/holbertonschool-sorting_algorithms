bd49d24ee071d0f82e15bd43dc61b18eee12b792
JohanVillard <styx.locke@gmail.com>
2024-08-09 17:17:25 +0200

    Don't swap same value

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -60,15 +60,20 @@ size_t partitioning(int *array, size_t low, size_t high, size_t size)
 			/* swap it with the greater element pointed by i */
 			i++;
 
-		/* swap element at i with element at j */
-		swap(&array[i], &array[j]);
-		print_array(array, size);
+			if (array[i] != array[j])
+			{
+				/* swap element at i with element at j */
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-
-	/* swap the pivot element with the greater element at i */
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	if (array[i + 1] != array[high])
+	{
+		/* swap the pivot element with the greater element at i */
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 	/* return the partition point */
 	return (i + 1);
 }
